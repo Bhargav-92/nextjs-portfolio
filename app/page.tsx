0af021c5ab5e5b424e1f124fda1db4c996e12b19
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-07 18:46:55 +0530

    feat:  added floating nave to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from '@/components/Hero';
+import { FloatingNav } from '@/components/ui/FloatingNav';
 import Image from 'next/image';
+import { FaHome } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <div>
+      <FloatingNav navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]} />
       <Hero />
     </div>
   );
